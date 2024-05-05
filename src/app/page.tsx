17f0f487cbbe4f157b27249ed94ba0cf9f995b76
syedJaleed <syedjaleedah29@gmail.com>
2024-05-05 19:29:46 +0530

    number changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -470,7 +470,7 @@ const ContactUs = () => {
               title="Take me to whatsapp"
               href="https://twitter.com/mannupaaji"
             >
-              <div onClick={() => window.open(`https://api.whatsapp.com/send?phone=${'6361991723'}`, '_blank')} className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+              <div onClick={() => window.open(`https://api.whatsapp.com/send?phone=${contactInfo.phNo}`, '_blank')} className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
                 <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
                   Click to connect us on WhatsApp
                 </h3>
