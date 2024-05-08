64c398703e053dcb74a3229e25e8445ca0787fb9
faizan ahmed <faizanahmadi2000@gmail.com>
2024-05-08 21:51:26 +0530

    html render

--- src/app/blogs/[title]/page.tsx
+++ src/app/blogs/[title]/page.tsx
@@ -1,14 +1,18 @@
 // pages/blogs/[title].tsx
 "use client";
+// Import necessary modules
 import { useEffect, useState } from 'react';
 import { firestore } from '../../../utils/firebase_service';
 import Link from "next/link";
+
+// Define interface for Blog
 interface Blog {
   title: string;
   content: string;
   category: string;
 }
 
+// Navbar component
 const Navbar = () => {
   return (
     <nav className="bg-opacity-30 bg-black backdrop-filter backdrop-blur-md py-4 fixed top-0 left-0 right-0 z-50">
@@ -26,12 +30,6 @@ const Navbar = () => {
           <li className="text-black-800 hover:text-red-600 cursor-pointer">
             <Link href="/">Home</Link>
           </li>
-          {/* <li className="text-black-800 hover:text-red-600 cursor-pointer">
-            <a id="about-link" href="#scanmadi-section">Projects</a>
-          </li>
-          <li className="text-black-800 hover:text-red-600 cursor-pointer">
-            <a id="services-link" href="#servicesection">Services</a>
-          </li> */}
           <li className="text-black-800 hover:text-red-600 cursor-pointer">
             <Link href="/blogs">Blogs</Link>
           </li>
@@ -43,17 +41,17 @@ const Navbar = () => {
     </nav>
   )
 }
+
+// BlogPage component
 const BlogPage = ({ params }: { params: { title: string } }) => {
   const [blog, setBlog] = useState<Blog | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [notFound, setNotFound] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log(params.title)
     if (params.title) {
       const fetchBlog = async () => {
         try {
-          console.log("asdasdas")
           const blogRef = await firestore.collection('blogs').where('id', '==', params.title).get();
           if (!blogRef.empty) {
             const blogData = blogRef.docs[0].data() as Blog;
@@ -75,21 +73,20 @@ const BlogPage = ({ params }: { params: { title: string } }) => {
     <div>
       <Navbar />
       <div className='mt-20'>
-      <div className="container mx-auto mt-8">
-        {loading ? (
-          <p>Loading...</p>
-        ) : notFound ? (
-          <p>Blog not found.</p>
-        ) : blog ? (
-          <div>
-            <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
-            <p className="text-gray-700">{blog.content}</p>
-            <p className="text-sm text-gray-500 mt-4">Category: {blog.category}</p>
-          </div>
-        ) : null}
+        <div className="container mx-auto mt-8">
+          {loading ? (
+            <p>Loading...</p>
+          ) : notFound ? (
+            <p>Blog not found.</p>
+          ) : blog ? (
+            <div>
+              <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
+              <div className="" dangerouslySetInnerHTML={{ __html: blog.content }} />
+              <p className="text-sm text-gray-500 mt-4">Category: {blog.category}</p>
+            </div>
+          ) : null}
+        </div>
       </div>
-      </div>
-      
     </div>
   );
 };
